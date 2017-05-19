e02bd3b7fdf92caca9515fc4c95cdb09cfa67f8e
Joe Hildebrand <joe-github@cursive.net>
2017-05-19 13:14:11 -0600

    Add web-ext support for signing

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
+all: sign
+
 hackernews-enhancement.zip: background.js enhance.js manifest.json
 	$(RM) $@
 	zip $@ $^
+
+sign:
+	web-ext sign -a docs
 
 clean:
 	$(RM) *.zip
--- background.js
+++ background.js
@@ -1,8 +1,10 @@
+'use strict';
+
 function handleMessage(request, sender, sendResponse) {
   browser.tabs.create({
     url: request,
     active: false
-  })
+  });
   sendResponse('done');
 }
 
